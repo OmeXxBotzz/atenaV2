09bcfa6d68270c2abb49afe949f2441d05ae2dd4
BapakLuPedo <92125020+BapakLuPedo@users.noreply.github.com>
2021-10-22 13:37:56 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,72 @@
+// Bismillahirrahmanirrahim
+// thank you to ALLAH Swt
+// thank you to Nurutomo as wabot-aq
+// thank you to ariffb as stikerinbot
+// thank you to botstylee
+// thank you to bochilgaming as games-wabot
+// thank you to benni ismael
+// thank you to zerochanBot
+// thank you to fernazer
+// thank you to MikeBot Dev Team
+// thank you to ALL Bot creator
+// and thanks you to who support my Bot
+
+global.linkGC = ['https://chat.whatsapp.com/INua3dEj25B0eFrHma0OsC', 'https://chat.whatsapp.com/B9bGT4rmw8PFnTYCYRcNab', 'https://chat.whatsapp.com/B9bGT4rmw8PFnTYCYRcNab'] // ganti jadi group lu
+global.owner = ['6282248192917', '628114811363', '62822481929171'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  dhnjing: 'https://dhnjing.xyz',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey' (apikey kalian)
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://api.xteam.xyz': 'Dawnfrostkey',
+  'https://api.lolhuman.xyz': 'pemburuBansos',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'ShiroNeko',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://fxc7-api.herokuapp.com': 'fxc7COOL',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+global.packname = '©Atena 𝚋𝚢 𝙺𝚑𝚊𝚎𝚕𝚂𝚊𝚗' // ganti aja
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn' // ganti aja
+
+// silakan di ganti klo mau
+global.wait = '_*Wait...*_'
+global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/INua3dEj25B0eFrHma0OsC'
+global.nsfw = 'Fitur NSFW Dimatikan.'
+global.eror = '_*Server Error*_'
+global.watermark = '© Atena'
+
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+
+// tingkat kesulitan, semakin tinggi semakin susah
+global.multiplier = 16 // The higher, The harder levelup
+
+//*****************PEMBATAS*********************
+// JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
