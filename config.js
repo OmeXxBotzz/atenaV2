ba9de01d1ab49f0d6dc00c4ee84c0713f1b65938
BapakLuPedo <92125020+BapakLuPedo@users.noreply.github.com>
2021-11-22 13:13:16 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
-// Source Code from NekoBotz (DanishGanz)
-
-global.linkGC = ['https://chat.whatsapp.com/INua3dEj25B0eFrHma0OsC', 'https://chat.whatsapp.com/B9bGT4rmw8PFnTYCYRcNab', 'https://chat.whatsapp.com/B9bGT4rmw8PFnTYCYRcNab'] // ganti jadi group lu
+//──────────────────────────────────────────────────\\
+//──────────────────────────────────────────────────\\
+//											© KhaelSan - © AtenaBot													  \\
+//──────────────────────────────────────────────────\\
+//──────────────────────────────────────────────────\\
+global.linkGC = ['https://chat.whatsapp.com/I0mpMnpPncB9qzAAtd6c14', 'https://chat.whatsapp.com/FtztbBRlAis2o9r6ebLhzj', 'https://chat.whatsapp.com/IfZhXNWaOCMH69jtHbv9xk'] // ganti jadi group lu
 global.owner = ['6282248192917', '62822481929171', '6281212924676'] // Put your number here
 global.mods = ['6285904398191'] // Want some help?
-global.prems = ['6283819142257', '6285695484734', '6285604708139'] // Premium user has unlimited limit
+global.prems = ['6283819142257', '6285695484734', '6285604708139', '6285904398191'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
+  hanzz: 'https://hanzzganz.herokuapp.com',
   hardianto: 'https://hardianto-chan.herokuapp.com',
   neoxr: 'https://neoxr-api.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -20,6 +24,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey' (apikey kalian)
+  'https://hanzzganz.herokuapp.com': 'HanzzGanz'
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://api.xteam.xyz': 'Dawnfrostkey',
@@ -32,17 +37,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-//global.packname = '© Atena Bot\nwa.me/628114811363\n-\n-\n𝚋𝚢 𝙺𝚑𝚊𝚎𝚕𝚂𝚊𝚗\n\n\n\n\n\nⓈⓐⓞⓣⓞⓜⓔ\nⒶⓣⓔⓝⓐ\n\n\nFollow juga IG Owner: @khaelll._'
-global.packname = ''
-global.author = '© Atena Bot\nwa.me/628114811363\n┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n┇\n┇					Ⓢⓐⓞⓣⓞⓜⓔ\n┇							Ⓐⓣⓔⓝⓐ\n┇\n┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\nғᴏʟʟᴏᴡ ɪɢ ᴏᴡɴᴇʀ: @ᴋʜᴀᴇʟʟʟ._\n𝚋𝚢 𝙺𝚑𝚊𝚎𝚕𝚂𝚊𝚗\n'
-//global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn' // ganti aja
+global.packname = '© Atena Bot\nwa.me/628114811363\n-\n-\n𝚋𝚢 𝙺𝚑𝚊𝚎𝚕𝚂𝚊𝚗\n\n\n\n\n\nⓈⓐⓞⓣⓞⓜⓔ\nⒶⓣⓔⓝⓐ\n\n\nFollow juga IG Owner: @khaelll._'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn'
+//global.packname = ''
+//global.author = '© Atena Bot\nwa.me/628114811363\n┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n┇\n┇					Ⓢⓐⓞⓣⓞⓜⓔ\n┇							Ⓐⓣⓔⓝⓐ\n┇\n┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\nғᴏʟʟᴏᴡ ɪɢ ᴏᴡɴᴇʀ: @ᴋʜᴀᴇʟʟʟ._\n𝚋𝚢 𝙺𝚑𝚊𝚎𝚕𝚂𝚊𝚗\n'
 
 // msg
 global.wait = '_*Ｗａｉt...*_'
 global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!'
 global.nsfw = 'Fitur NSFW Dimatikan.'
 global.eror = '_*Server Error*_'
-global.watermark = '© Atena'
+global.watermark = '© AtenaBot'
 
 global.imagebot = 'https://telegra.ph/file/5b94da0fc1fafa4aa1be7.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
@@ -50,8 +55,8 @@ global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=tr
 // tingkat kesulitan, semakin tinggi semakin susah
 global.multiplier = 64
 
-//*****************PEMBATAS*********************
-// JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
+//*****************PEMBATAS*********************\\
+// DON'T CHANGE OR REMOVE!, IF ERROR DON'T ASK ME!
 
 let fs = require('fs')
 let chalk = require('chalk')
