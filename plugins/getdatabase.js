7c581d677bd4f544aea2b6295c4224c223686eec
CAPTbarcer <50859329+CAPTbarcer@users.noreply.github.com>
2021-12-06 18:30:47 +0900

    Add files via upload

--- plugins/getdatabase.js
+++ plugins/getdatabase.js
@@ -1,8 +1,21 @@
 let fs = require('fs')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	var database = fs.readFileSync('./database.json')
-		conn.reply(m.chat, `Database: ${date}`, null)
-			conn.sendFile(m.chat, database, "database.json", "", m)
+		let d = new Date(new Date + 3600000)
+			let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+			conn.reply(m.chat, `Database: ${date}`, null)
+				conn.sendFile(m.chat, database, "database.json", "", null)
 }
 
 handler.help = ['getdabase']
