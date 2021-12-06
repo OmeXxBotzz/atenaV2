6fee685afc1ed16f6a12048f6d6f4e065577f570
CAPTbarcer <50859329+CAPTbarcer@users.noreply.github.com>
2021-12-06 10:13:39 +0900

    Add files via upload

--- plugins/ig2.js
+++ plugins/ig2.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 const axios = require('axios')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CNTwipplVtv/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
+  if (!args[0].match(/https:\/\/www.instagram.com/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
   m.reply(wait)
   let res = await axios(global.API('lolhum', '/api/instagram2', { url: args[0] }, 'apikey'))
   let json = res.data.result
